9d92cc7c9dd9086f643a2e81fd26026f5bb8d9c1
David Mathie <imdavidj13@gmail.com>
2026-06-03 17:16:32 +0100

    Move treasure sign beyond hero edge

--- app/child/page.tsx
+++ app/child/page.tsx
@@ -215,7 +215,7 @@ if (childData) {
 </div>
     </div>
 
-   <div className="hidden md:flex items-center justify-end translate-x-20">
+   <div className="hidden md:flex items-center justify-end translate-x-10">
   <div
   className="relative h-[380px] w-[380px] bg-contain bg-center bg-no-repeat"
     style={{
